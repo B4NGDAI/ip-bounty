fa1bbc258323774773700cf0dd218fcd524a740f
B4NGDAI <adnanrahmatulah@gmail.com>
2023-07-21 01:52:49 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,9 +2,9 @@ fx_version 'cerulean'
 game "rdr3"
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 lua54 'yes'
+author "adnanberandai"
 
 shared_scripts {
-    '@ox_lib/init.lua',
     'config.lua',
 }
 
